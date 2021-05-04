7fb54d3f432bab44e3f7f690d3e10767bd11e5d8
Marc Nehme <201610021@ua.edu.lb>
2021-05-04 20:36:46 +0300

    Created file for constant colors and text styles

---
+++ lib/shared/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Colors of the App
+  static const Color PRIMARY_BLUE = Color(0xFF2395C8);
+  static const Color SECONDARY_BLUE = Color(0xFFDFEEF5);
+  static const Color PRIMARY_ORANGE = Color(0xFFFFE45D);
+  static const Color SECONDARY_ORANGE = Color(0xFFFEBB47);
+
+  //Different font sizes
+  static const TextStyle AppBarText =
+      TextStyle(fontSize: 20.0, color: Colors.white);
+}
