fde5d6dd5476a24fdcba6a8ce4d4e5351e77db39
Marc Nehme <201610021@ua.edu.lb>
2021-05-05 21:25:44 +0300

    Swapped the primary and secondary orange colors

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,8 +4,8 @@ class Constants {
   //Colors of the App
   static const Color PRIMARY_BLUE = Color(0xFF2395C8);
   static const Color SECONDARY_BLUE = Color(0xFFDFEEF5);
-  static const Color PRIMARY_ORANGE = Color(0xFFFFE45D);
-  static const Color SECONDARY_ORANGE = Color(0xFFFEBB47);
+  static const Color PRIMARY_ORANGE = Color(0xFFFEBB47);
+  static const Color SECONDARY_ORANGE = Color(0xFFFFE45D);
   static const Color SAFEAREA_COLOR = Color(0xD92395C8);
 
   //Different font sizes
