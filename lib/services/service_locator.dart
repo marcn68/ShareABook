833210430cb91a0ca23e50a31e0aed94c55069c0
Marc Nehme <201610021@ua.edu.lb>
2021-05-05 21:24:34 +0300

    Created the service locator file

---
+++ lib/services/service_locator.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+import 'package:share_a_book/business_logic/view_models/add_book_viewmodel.dart';
+import 'package:share_a_book/services/book_add/book_add_service.dart';
+import 'book_add/book_add_service_implementation.dart';
+import 'web_api/web_api.dart';
+import 'web_api/web_api_implementation.dart';
+
+GetIt serviceLocator = GetIt.instance;
+
+void setupServiceLocator() {
+  //services registrations
+  serviceLocator.registerLazySingleton<WebApi>(() => WebApiImplementation());
+  serviceLocator.registerLazySingleton<BookAddService>(
+      () => BookAddServiceImplementation());
+
+  //view models registrations
+  serviceLocator.registerFactory<AddBookViewModel>(() => AddBookViewModel());
+  //serviceLocator.registerFactory<AddBookViewModel>(() => AddBookViewModel());
+}
