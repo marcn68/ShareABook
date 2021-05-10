35b6370b454ef1b59f28be88096cbec09d12c6b4
Marc Nehme <201610021@ua.edu.lb>
2021-05-10 21:37:03 +0300

    Create backend api implementations and register them to the service locator

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:share_a_book/business_logic/view_models/add_book_viewmodel.dart';
 import 'package:share_a_book/services/book_add/book_add_service.dart';
+import 'package:share_a_book/services/web_api/backend_api.dart';
+import 'package:share_a_book/services/web_api/backend_api_implementation.dart';
 import 'book_add/book_add_service_implementation.dart';
 import 'web_api/web_api.dart';
 import 'web_api/web_api_implementation.dart';
@@ -10,6 +12,8 @@ GetIt serviceLocator = GetIt.instance;
 void setupServiceLocator() {
   //services registrations
   serviceLocator.registerLazySingleton<WebApi>(() => WebApiImplementation());
+  serviceLocator
+      .registerLazySingleton<BackendApi>(() => BackendApiImplementation());
   serviceLocator.registerLazySingleton<BookAddService>(
       () => BookAddServiceImplementation());
 
---
+++ lib/services/web_api/backend_api_implementation.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:share_a_book/business_logic/models/book.dart';
+import 'package:share_a_book/services/web_api/backend_api.dart';
+import 'package:http/http.dart' as http;
+
+class BackendApiImplementation implements BackendApi {
+  final _host = "openlibrary.org";
+  final _path = "api/books";
+  final Map<String, String> _headers = {'Accept': 'application/json'};
+
+  @override
+  Future<bool> createBook(Book book) async {
+    final response = await http.post(
+      Uri.https(_host, _path),
+      headers: _headers,
+      body: jsonEncode(<String, dynamic>{
+        'book': book.toJson(),
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
