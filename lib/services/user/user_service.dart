428fde91d88cf4b5888bb7bd89ba28c1fe29d62c
Elie Haddad <elie.g.haddad52@gmail.com>
2021-05-12 18:35:30 +0300

    Fixed Abstraction class function parameter to match implementation

--- lib/services/user/user_service.dart
+++ lib/services/user/user_service.dart
@@ -1,5 +1,5 @@
 abstract class UserService {
   Future updateUser({String userImage, String fullName});
   Future deleteUser();
-  Future changePassword({String newPassword});
+  Future changePassword({String newPassword, String oldPassword});
 }
