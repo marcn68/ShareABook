1f8aa7971fe8006e105df9185740fa3f730511a6
Marc Nehme <201610021@ua.edu.lb>
2021-05-15 22:15:06 +0300

    Created userbook and bookdocument models

---
+++ lib/business_logic/models/book_document.dart
@@ -0,0 +1,8 @@
+import 'package:share_a_book/business_logic/models/user_book.dart';
+
+class BookDocument {
+  String objectId;
+  UserBook userBook;
+
+  BookDocument({this.objectId, this.userBook});
+}
---
+++ lib/business_logic/models/user_book.dart
@@ -0,0 +1,8 @@
+import 'book.dart';
+
+class UserBook {
+  Book book;
+  String userId;
+
+  UserBook({this.book, this.userId});
+}
