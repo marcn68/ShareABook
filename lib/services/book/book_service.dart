d2a7b05a6006471434b807eae6e727b4715c5956
Marc Nehme <201610021@ua.edu.lb>
2021-05-16 14:31:16 +0300

    Added book service functionalities

--- lib/services/book/book_service.dart
+++ lib/services/book/book_service.dart
@@ -2,6 +2,12 @@ import 'package:share_a_book/business_logic/models/book.dart';
 
 abstract class BookService {
   Future<List<Book>> getBooksByISBN(isbn);
-
+  // Create a new Book Object
   Future<bool> sendBookInfo(book);
+  // Get a list of the most popular books
+  Future<List<Book>> popularBooks();
+  // Get a list of all the user books
+  Future<List<Book>> userBookList();
+  // Get a list of all the books based on the search query
+  Future<List<Book>> searchBookByTerm(searchTerm);
 }
--- lib/services/book/book_service_implementation.dart
+++ lib/services/book/book_service_implementation.dart
@@ -1,4 +1,6 @@
 import 'package:share_a_book/business_logic/models/book.dart';
+import 'package:share_a_book/business_logic/models/user_book.dart';
+import 'package:share_a_book/services/authentication/auth_service.dart';
 import 'package:share_a_book/services/service_locator.dart';
 import 'package:share_a_book/services/web_api/backend_api.dart';
 import 'package:share_a_book/services/web_api/web_api.dart';
@@ -9,14 +11,32 @@ class BookServiceImplementation implements BookService {
   WebApi _webApi = serviceLocator<WebApi>();
   BackendApi _backendApi = serviceLocator<BackendApi>();
 
+  List<Book> books = [];
+
   @override
   Future<List<Book>> getBooksByISBN(isbn) async {
     List<Book> webData = await _webApi.getBookById(isbn);
     return webData;
   }
 
+  @override
   Future<bool> sendBookInfo(book) async {
     bool response = await _backendApi.createBook(book);
     return response;
   }
+
+  @override
+  Future<List<Book>> popularBooks() async {
+    // books = await _backendApi.getBooksOfTheWeek();
+    // return books;
+  }
+
+  @override
+  Future<List<Book>> searchBookByTerm(searchTerm) {
+    // books =
+    // return books;
+  }
+
+  @override
+  Future<List<Book>> userBookList() {}
 }
