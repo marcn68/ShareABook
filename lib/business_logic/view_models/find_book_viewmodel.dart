ec5b65e9a1c81bced22c56250e846f1472fa5eea
Marc Nehme <201610021@ua.edu.lb>
2021-05-16 18:34:08 +0300

    Changed books to bookDocuments

--- lib/business_logic/view_models/find_book_viewmodel.dart
+++ lib/business_logic/view_models/find_book_viewmodel.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/foundation.dart';
-import 'package:share_a_book/business_logic/models/book.dart';
+import 'package:share_a_book/business_logic/models/book_document.dart';
 
 class FindBookViewModel extends ChangeNotifier {
   String searchInput;
-  List<Book> _books = [];
+  List<BookDocument> _bookDocuments = [];
 
-  List<Book> get books {
-    return _books;
+  List<BookDocument> get bookDocuments {
+    return _bookDocuments;
   }
 
   void searchBookByTerm() {}
