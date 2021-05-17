32264f92da6a67f9ecead43c6ba83ec3df9fc545
Marc Nehme <201610021@ua.edu.lb>
2021-05-17 13:14:46 +0300

    Added return messages and snackbar for the change password function

--- lib/business_logic/view_models/change_password_viewmodel.dart
+++ lib/business_logic/view_models/change_password_viewmodel.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:share_a_book/app/app.router.dart';
 import 'package:share_a_book/services/service_locator.dart';
 import 'package:share_a_book/services/user/user_service.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class ChangePasswordViewModel extends ChangeNotifier {
   final UserService _userService = serviceLocator<UserService>();
 
-  // String oldPass;
-  // String newPass;
-  // String confirmPass;
   String msg;
-
+  SnackBar snackBar;
   TextEditingController oldPassController;
   TextEditingController newPassController;
   TextEditingController confirmPassController;
 
-  void checkOldPass() {}
+  //void checkOldPass() {}
 
-  void checkPassword() {
+  void checkPassword() async {
     if (newPassController.text.toString() ==
         confirmPassController.text.toString()) {
-      _userService.changePassword(
-          newPassword: newPassController.text.toString(),
-          oldPassword: oldPassController.text.toString());
+      try {
+        dynamic response = await _userService.changePassword(
+            newPassword: newPassController.text.toString(),
+            oldPassword: oldPassController.text.toString());
+        msg = "Password Changed Succesfully";
+        print(response);
+        oldPassController.clear();
+        newPassController.clear();
+        confirmPassController.clear();
+        //await serviceLocator<NavigationService>().navigateTo(Routes.myApp);
+      } catch (e) {
+        msg = e.toString();
+        print(e.toString());
+      }
     } else {
       msg = "Your passwords do not match";
     }
