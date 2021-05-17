88fd04d16428747b57dac7ab5e0f2a011023cc2d
Marc Nehme <201610021@ua.edu.lb>
2021-05-17 20:38:13 +0300

    Refactored the attribute and function to use BookDocument instead of Book

--- lib/business_logic/view_models/home_viewmodel.dart
+++ lib/business_logic/view_models/home_viewmodel.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/foundation.dart';
-import 'package:share_a_book/business_logic/models/book.dart';
+import 'package:share_a_book/business_logic/models/book_document.dart';
 import 'package:share_a_book/services/book/book_service.dart';
 import 'package:share_a_book/services/service_locator.dart';
-import 'package:share_a_book/services/web_api/backend_api.dart';
 
 class HomeViewModel extends ChangeNotifier {
   BookService _bookService = serviceLocator<BookService>();
 
-  List<Book> _books = [];
+  List<BookDocument> _bookDocuments = [];
 
-  List<Book> get books {
-    return _books;
+  List<BookDocument> get bookDocuments {
+    return _bookDocuments;
   }
 
   Future getPopularBooks() async {
-  /*     Book book = Book(isbn: "0", title: "bookTitle", authors: ["authors"], publishDate: "21-5-2021", publishers: ["publishers"], subjects: ["subjects"], numberOfPages: "5",cover: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb6%2FImage_created_with_a_mobile_phone.png%2F1200px-Image_created_with_a_mobile_phone.png&imgrefurl=https%3A%2F%2Fsimple.wikipedia.org%2Fwiki%2FPicture&tbnid=gxFxsvFBmxeZ9M&vet=12ahUKEwi5_u_kuMbwAhVRQRoKHZw2DyoQMygAegUIARDSAQ..i&docid=BNDmYf01iceXQM&w=1200&h=900&q=image&ved=2ahUKEwi5_u_kuMbwAhVRQRoKHZw2DyoQMygAegUIARDSAQ" );
-    _books.add(book);
-    _books.add(book);
-    _books.add(book);*/
-
-    _books = await _bookService.popularBooks();
+    _bookDocuments = await _bookService.popularBooks();
     notifyListeners();
   }
 }
