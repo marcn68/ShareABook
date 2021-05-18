a0f8467701c44224a3ec6529cdc29998a1b5cc15
Marc Nehme <201610021@ua.edu.lb>
2021-05-19 00:55:36 +0300

    Create a conversation id function

--- lib/business_logic/view_models/user_detail_viewmodel.dart
+++ lib/business_logic/view_models/user_detail_viewmodel.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:share_a_book/services/chat/chat_service.dart';
+import 'package:share_a_book/services/service_locator.dart';
 
 class UserDetailViewModel extends ChangeNotifier {
+  final ChatService _chatService = serviceLocator<ChatService>();
   List<String> emails = [];
 
   void sendEmail(context, emailRecipient) async {
@@ -11,5 +14,10 @@ class UserDetailViewModel extends ChangeNotifier {
     try {
       await FlutterEmailSender.send(email);
     } catch (e) {}
+  }
+
+  String createConversation(uid, id) {
+    String convoId = _chatService.getConversationId(uid, id);
+    return convoId;
   }
 }
